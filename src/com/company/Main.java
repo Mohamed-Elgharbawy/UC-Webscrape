1426799718c430da5133bcca3e82f783cd1155cb
Mohamed-Elgharbawy <30507689+Mohamed-Elgharbawy@users.noreply.github.com>
2018-07-20 15:05:07 -0700

    Updated old main

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        UcUserInput ui = new UcUserInput();
+        ui.userInput();
+    }
+
+}
